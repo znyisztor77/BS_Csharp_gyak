f95b789bfa4dad8a9ddab5d6b0bd8616cf2556e9
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-16 15:46:30 +0100

    12.16

---
+++ A17_PenztarosjJutalom/A17_PenztarosjJutalom/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A17_PenztarosjJutalom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double szam = szambeker("Kérem a napi bevételt!: ");
+            Console.WriteLine($"A pénztáros napi jutalma: {Math.Round(szam / 100) * 5}ft");
+            Console.ReadKey();
+
+        }
+
+        private static int szambeker(string v)
+        {
+
+            double szam;
+            Console.Write(v);
+
+
+
+            while (!double.TryParse(Console.ReadLine(), out szam))
+            {
+                Console.WriteLine("Nem megfelelő adatbevitel!");
+                Console.Write(v);
+            }
+            return (int)szam;
+           
+        }
+    }
+}
