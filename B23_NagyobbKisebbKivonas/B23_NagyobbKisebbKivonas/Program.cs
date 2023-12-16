8e88fc02df2dfa7410e99506de60f3305b96c565
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-16 15:53:52 +0100

    12.16

---
+++ B23_NagyobbKisebbKivonas/B23_NagyobbKisebbKivonas/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B23_NagyobbKisebbKivonas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam1;
+            int szam2;
+            szam1 = szamBeker("Kérek egy számot: ");
+            szam2 = szamBeker("Kérek egy másik számot: ");
+            KulonbsegEsCsere(szam1, szam2);
+            Console.WriteLine($"A két szám különbsége: {KulonbsegEsCsere(szam1, szam2)}");
+            Console.ReadKey();
+        }
+        private static int szamBeker(string v)
+        {
+            int szam;
+            Console.WriteLine(v);
+            while (!int.TryParse(Console.ReadLine(), out szam))
+            {   
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Nem megfelelő bevitel!");
+                Console.ForegroundColor= ConsoleColor.Gray;
+                Console.WriteLine(v);
+
+            }
+            return szam;
+        }
+
+        private static int KulonbsegEsCsere(int szam1, int szam2)
+        {
+
+            int csere;
+
+            if (szam2 < szam1)
+            {
+                csere = szam2;
+                szam2 = szam1;
+                szam1 = csere;
+
+            }
+            return szam2 - szam1;
+        }
+    }
+}
