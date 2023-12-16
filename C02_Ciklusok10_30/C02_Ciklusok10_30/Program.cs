9cec9d1d82ae6f3cb1d5ad0d4d9d5470353b542f
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-16 17:00:20 +0100

    12.16

---
+++ C02_Ciklusok10_30/C02_Ciklusok10_30/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C02_Ciklusok10_30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 és 30 közötti páros számok:");
+            for (int i = 10; i < 30; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(" " + i);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
