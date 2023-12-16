641b4390132cc76317c7af4934ea74d018af1746
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-16 16:55:55 +0100

    12.16

---
+++ C01_Ciklusok10_20/C01_Ciklusok10_20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01_Ciklusok10_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 és 20 közötti számok");
+            for (int i = 11; i < 20; i++)
+            {
+                Console.Write(" " + i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
