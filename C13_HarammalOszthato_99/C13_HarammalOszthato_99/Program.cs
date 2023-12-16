e359e3b1ae16c015b66c9767627407256ea5b21b
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-16 18:12:49 +0100

    12.16

---
+++ C13_HarammalOszthato_99/C13_HarammalOszthato_99/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C13_HarammalOszthato_99
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("99-től viszafelé a 3-al osztható számok");
+
+            int i;
+
+            for (i = 99; i > 0; i--)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
