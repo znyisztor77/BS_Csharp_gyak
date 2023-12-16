4730740cbf4a1e7621e7dfcbc09588e1a45f894c
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-16 15:21:35 +0100

    12.16

---
+++ A01_Ketszam_kulonbseg/A01_Ketszam_kulonbseg/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A01_Ketszam_kulonbseg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double szam1, szam2;
+            szam1 = szamBeker("Kérek egy számot:");
+            szam2 = szamBeker("Kérek még egy számot");
+            Console.WriteLine($"A két szám összege {szam1 + szam2}, a különbsége {szam1 - szam2}");
+           
+            Console.ReadKey();
+        }
+        private static double szamBeker(string v)
+        {
+            Console.WriteLine(v);
+            double szam = 0;
+            while (!double.TryParse(Console.ReadLine(), out szam))
+            {
+                Console.WriteLine("Nem megfelelő bevitel");
+                Console.WriteLine(v);
+
+            }
+            return szam;
+        }
+    }
+}
