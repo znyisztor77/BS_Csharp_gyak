36311236f723104fffa6edf0aff87bd4e598680d
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-16 17:55:37 +0100

    12.16

---
+++ C06_Negyzetszamol/C06_Negyzetszamol/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C06_Negyzetszamol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A program kiírja 1-20-ig a számokat és mellé a négyzetüket.");
+            negyzet_Szamol(20);
+            Console.ReadKey();
+        }
+        private static void negyzet_Szamol(int v)
+        {
+            int i;
+            for (i = 1; i < v + 1; i++)
+            {
+                int negyzet;
+                negyzet = i * i;
+                Console.WriteLine($"A {i} négyzete {negyzet}. ");
+            }
+        }
+    }
+}
