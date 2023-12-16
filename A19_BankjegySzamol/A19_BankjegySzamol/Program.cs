7abf53a50193c09cddaad57b975cb7600ded4532
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-16 15:48:32 +0100

    12.16

---
+++ A19_BankjegySzamol/A19_BankjegySzamol/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A19_BankjegySzamol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem ad meg hogy melyik cimletből hány darab van");
+            osszedAd();
+            Console.ReadKey();
+        }
+        public static void osszedAd()
+        {
+            int[] cimletek = { 500, 1000, 2000, 5000, 10000, 20000 };
+            int szamlalo = 0;
+            int osszeg = 0;
+            int darab;
+            do
+            {
+
+                for (int i = 0; i < cimletek.Length; i++)
+                {
+                    Console.Write($"Hány darab van a {cimletek[i]} forintosbol?: ");
+
+                    while (!int.TryParse(Console.ReadLine(), out darab))
+                    {
+                        Console.WriteLine("Nem memgfelelő bevitel, kérem a darab számot!");
+                        Console.Write($"Hány darab van a {cimletek[i]} forintosbol?: ");
+
+                    }
+                    osszeg += darab * cimletek[i];
+
+                }
+
+                szamlalo++;
+
+            }
+            while ((szamlalo == cimletek.Length));
+            Console.WriteLine($"\n A mai bevétel {osszeg}ft volt.");
+
+
+        }
+    }
+}
