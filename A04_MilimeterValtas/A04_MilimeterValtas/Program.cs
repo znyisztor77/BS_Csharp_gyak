2c3e0d54c42bbd5efdcb416f630a15d2b8557830
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-22 08:42:29 +0100

    12.22

---
+++ A04_MilimeterValtas/A04_MilimeterValtas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A04_MilimeterValtas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal adat;
+            Console.WriteLine("Miliméterben megadott érték váltása méterre,deciméterre, centiméterre és miliméterre.");
+            adat = adatBeker("Kérem az átváltandó értéket: ");
+
+            decimal meter = adat / 1000;
+            decimal decimeter = adat / 100;
+            decimal centimeter = adat / 10;
+            Console.WriteLine($"{adat}mm {meter}m");
+            Console.WriteLine($"{adat}mm {decimeter}dm");
+            Console.WriteLine($"{adat}mm {centimeter}cm");
+            Console.ReadKey();
+        }
+
+        private static decimal adatBeker(string v)
+        {
+            Console.Write(v);
+            decimal adat;
+            while(!decimal.TryParse(Console.ReadLine(), out adat))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Hibás adat bevitel!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(v);
+            }
+            return adat;
+        }
+    }
+}
