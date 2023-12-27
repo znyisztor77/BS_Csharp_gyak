b3f86d0fcf57e5162e012d208c3aa9db312faed2
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-27 18:10:44 +0100

    12.27

---
+++ A10_KockaFelszinTerfogat/A10_KockaFelszinTerfogat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A10_KockaFelszinTerfogat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Négyzet felszín, térfogat számítása.");
+            double oldal;
+            oldal = sugarBeker("Kérem add meg négyzet oldalhosszát: ");
+            double felszin = 6 * Math.Pow(oldal, 2);
+            double terfogat = Math.Pow(oldal, 3);
+            Console.WriteLine($"A négyzet felszine: {Math.Round(felszin, 2)}");
+            Console.Write($"A négyzet térfogata: {Math.Round(terfogat, 2)}");
+            Console.ReadKey();
+        }
+
+        private static double sugarBeker(string v)
+        {
+            double oldal;
+            Console.Write(v);
+            while (!double.TryParse(Console.ReadLine(), out oldal))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Hibás bevitel!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(v);
+
+            }
+            return oldal;
+        }
+    }
+}
