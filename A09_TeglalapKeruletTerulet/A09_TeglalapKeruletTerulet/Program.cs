260edd96afc06f840a00c2b3605455d2e27faf13
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-27 18:38:36 +0100

    12.27

---
+++ A09_TeglalapKeruletTerulet/A09_TeglalapKeruletTerulet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A09_TeglalapKeruletTerulet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Téglalap kerület, terület számítása.");
+            double aOldal;
+            double bOldal;
+            aOldal = sugarBeker("Kérem add meg a téglalap egyik oldalának hosszát: ");
+            bOldal = sugarBeker("Kérem add meg a téglalap másik oldalának hosszát: ");
+            double kerulet = 2*(aOldal+bOldal);
+            double terulet = aOldal * bOldal;
+            Console.WriteLine($"A négyzet kerülete: {Math.Round(kerulet, 2)}");
+            Console.Write($"A négyzet területe: {Math.Round(terulet, 2)}");
+            Console.ReadKey();
+        }
+        private static double sugarBeker(string v)
+        {
+            double oldal;
+            Console.Write(v);
+            while (!double.TryParse(Console.ReadLine(), out oldal))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Hibás bevitel!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(v);
+
+            }
+            return oldal;
+        }
+    }
+}
