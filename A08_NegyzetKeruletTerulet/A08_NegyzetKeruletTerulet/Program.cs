86eb6ef43cc2daaedfa39327c56e48e4f5dd71e1
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-27 17:15:05 +0100

    12.27

---
+++ A08_NegyzetKeruletTerulet/A08_NegyzetKeruletTerulet/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A08_NegyzetKeruletTerulet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Négyzet kerület, terület számítása.");
+            double oldal;
+            oldal = sugarBeker("Kérem add meg négyzet oldalhosszát: ");
+            double kerulet = 4* oldal;
+            double terulet = oldal * oldal;
+            Console.WriteLine($"A négyzet kerülete: {Math.Round(kerulet, 2)}");
+            Console.Write($"A négyzet területe: {Math.Round(terulet, 2)}");
+            Console.ReadKey();
+        }
+        private static double sugarBeker(string v)
+        {
+            double oldal;
+            Console.Write(v);
+            while (!double.TryParse(Console.ReadLine(), out oldal))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Hibás bevitel!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(v);
+
+            }
+            return oldal;
+        }
+    }
+}
