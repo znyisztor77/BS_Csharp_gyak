378e0d753fc2bd027160c5b37952b4f3df08552f
Nyisztor Zoltán <znyisztor77@gmail.com>
2023-12-28 09:57:28 +0100

    12.28

---
+++ A15_SzamtaniKozep/A15_SzamtaniKozep/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A15_SzamtaniKozep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Számtani közép számolás.");
+            int szamElso;
+            int szamMasodik;
+            szamElso = szamBeker("Kérem az első számot: ");
+            szamMasodik = szamBeker("Kérem a második számot: ");
+            Console.WriteLine($"A két szám számtani közepe: {(szamElso+szamMasodik)/2}");
+            Console.ReadKey();
+        }
+
+        private static int szamBeker(string v)
+        {
+            
+            int szam;
+            while (true)
+            {
+                Console.Write(v);
+                try
+                {
+                    int.TryParse(Console.ReadLine(), out szam);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Hibás adat");
+                }
+
+            }
+            return szam;
+        }
+    }
+}
